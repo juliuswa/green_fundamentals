562a910c354f38547f4ab48a2f4fecc1afb69cc4
Alex <alexschwind@gmail.com>
2024-05-05 12:19:25 +0200

    restore square_no_sensors

--- src/square_no_sensors.cpp
+++ src/square_no_sensors.cpp
@@ -20,39 +20,39 @@ void stopDriving() {
     ros::shutdown();
 }
 
-// int main(int argc, char **argv)
-// {
-//     ros::init(argc, argv, "square_no_sensors");
-//     ros::NodeHandle n;
-//     ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
-
-//     for(int i = 0; i < 5; i++) {
-// 	    for(int i = 0; i < 4; i++) {
-//         	drive(diffDrive, 8, 8);
-//         	ros::Duration(4.0).sleep();
-//         	drive(diffDrive, 10, -10);
-// 		    ros::Duration(0.62).sleep();
-//     	}
-//     }
-//     stopDriving();
-//     return 0;
-// }
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "square_no_sensors");
     ros::NodeHandle n;
     ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
-    
-    float wheel_speed = 10.;
-    float wheel_radius = 0.036;
-    float wheelbase = 0.235;
-    float s = 1.;
 
-    float D = M_PI * wheelbase / 4;
-
-    float rad_per_s = ((D/s)/wheel_radius);
-
-    drive(diffDrive, -rad_per_s, rad_per_s);
-    ros::Duration(s).sleep();
+    for(int i = 0; i < 5; i++) {
+	    for(int i = 0; i < 4; i++) {
+        	drive(diffDrive, 8, 8);
+        	ros::Duration(4.0).sleep();
+        	drive(diffDrive, 10, -10);
+		    ros::Duration(0.62).sleep();
+    	}
+    }
     stopDriving();
+    return 0;
 }
+
+// int main(int argc, char **argv) {
+//     ros::init(argc, argv, "square_no_sensors");
+//     ros::NodeHandle n;
+//     ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+    
+//     float wheel_speed = 10.;
+//     float wheel_radius = 0.036;
+//     float wheelbase = 0.235;
+//     float s = 1.;
+
+//     float D = M_PI * wheelbase / 4;
+
+//     float rad_per_s = ((D/s)/wheel_radius);
+
+//     drive(diffDrive, -rad_per_s, rad_per_s);
+//     ros::Duration(s).sleep();
+//     stopDriving();
+// }
