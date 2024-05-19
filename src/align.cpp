d03f56f3e86725b9b62fad24cf58a1283d8eb816
Green Robot <juliuswa98@gmail.com>
2024-05-19 09:54:19 +0000

    recovering after broken repository

--- src/align.cpp
+++ src/align.cpp
@@ -6,69 +6,36 @@
 #include "Eigen/Dense"
 #include "sensor_msgs/LaserScan.h"
 #include "create_fundamentals/DiffDrive.h"
+#include "green_fundamentals/Position.h"
 #include "green_fundamentals/DetectGrid.h"
+#include "green_fundamentals/DriveTo.h"
 #include "classes/driver.h"
 
-const float wheel_radius = 0.0308;
-const float wheel_base = 0.265;
-const float revolution_dist = wheel_radius * M_PI * 2;
+float x_position;
+float y_position;
+float theta_position;
 
-void stop_driving(int sig) {
-    ROS_DEBUG("stop_driving");
-    ros::NodeHandle n;
-    ros::ServiceClient diffDrive = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
+ros::ServiceClient driving_service;
 
-    create_fundamentals::DiffDrive srv;
-    srv.request.left = 0;
-    srv.request.right = 0;
-
-    diffDrive.call(srv);
-    ros::shutdown();
-    exit(1);
+void update_position(const green_fundamentals::Position::ConstPtr& position) {
+    x_position = position->x;
+    y_position = position->y;
+    theta_position = position->theta;
 }
 
-void rotate(float theta, Driver& driver) {
-    float turn_distance = wheel_base / 2 * theta;
+void call_drive_to_service(float x, float y, float theta, bool rotate) {
+    green_fundamentals::DriveTo drive_to_srv;
 
-    wheelCommand turn_command;
-    turn_command.left_wheel = -1 * turn_distance / revolution_dist * 2 * M_PI;
-    turn_command.right_wheel = turn_distance / revolution_dist * 2 * M_PI;
+    drive_to_srv.request.x_target = x;
+    drive_to_srv.request.y_target = y;
+    drive_to_srv.request.rotate = rotate;
+    drive_to_srv.request.theta_target = theta;
 
-    driver.execute_command(turn_command);
-}
+    ROS_INFO("Calling drive_to with (%f, %f, %d, %f)", x, y, rotate, theta);
 
-void align_at_position(Eigen::Vector2f position, float theta, Driver& driver) {
-    ROS_INFO("midpoint: offset = (%f, %f), theta = %f", position[0], position[1], theta * 180 / M_PI);
-
-    ROS_DEBUG("drive_to_relative_point"); 
-    std::deque<wheelCommand> drive_commands;
-
-    float midpoint_direction_theta = acos(position[0] / position.norm());
-    
-    float pre_turn_distance = wheel_base / 2 * midpoint_direction_theta;    
-    float drive_distance = position.norm();
-    float post_turn_distance = wheel_base / 2 * (theta - midpoint_direction_theta);  
-
-    wheelCommand pre_turn_command;
-    pre_turn_command.left_wheel = -1 * pre_turn_distance / revolution_dist * 2 * M_PI;
-    pre_turn_command.right_wheel = pre_turn_distance / revolution_dist * 2 * M_PI;
-    drive_commands.push_back(pre_turn_command);
-
-    wheelCommand drive_command;
-    drive_command.left_wheel = drive_distance / revolution_dist * 2 * M_PI;
-    drive_command.right_wheel = drive_distance / revolution_dist * 2 * M_PI;
-    drive_commands.push_back(drive_command);
-
-    wheelCommand post_turn_command;
-    post_turn_command.left_wheel = -1 * post_turn_distance / revolution_dist * 2 * M_PI;
-    post_turn_command.right_wheel = post_turn_distance / revolution_dist * 2 * M_PI;
-    drive_commands.push_back(post_turn_command);
-
-    while(!drive_commands.empty()) {
-        wheelCommand current_command = drive_commands.front();
-        drive_commands.pop_front();
-
-        driver.execute_command(current_command);
+    if (!driving_service.call(drive_to_srv))
+    {
+        ROS_ERROR("failed to call driver_service");
     }
 }
 
@@ -81,43 +48,45 @@ int main(int argc, char **argv)
         ros::console::notifyLoggerLevelsChanged();
     }
 
-    // ROS_DEBUG("Register grid_detector as subscriber"); 
-    // GridDetector grid_detector;
-    // ros::Subscriber sub = n.subscribe("scan_filtered", 1, &GridDetector::get_measurements, &grid_detector);
-
-    ROS_DEBUG("Register Driver as subscriber"); 
-    Driver driver(n);
-    ros::Subscriber sensorSub = n.subscribe("sensor_packet", 1, &Driver::calculate_wheel_speeds, &driver);
-
+    ros::Subscriber odometry_sub = n.subscribe("odometry", 1, update_position);
     ros::ServiceClient grid_detection_service = n.serviceClient<green_fundamentals::DetectGrid>("detect_grid");
-
-    signal(SIGINT, stop_driving);
+    driving_service = n.serviceClient<green_fundamentals::DriveTo>("drive_to");
 
     int retry_count = 0;
 
     while (retry_count < 4) {
+        ros::spinOnce();
 
-        green_fundamentals::DetectGrid srv;
+        green_fundamentals::DetectGrid detect_grid_srv;
 
-        if (!grid_detection_service.call(srv))
+        if (!grid_detection_service.call(detect_grid_srv))
         {
-            ROS_ERROR("detecting grid failed.");
+            ROS_ERROR("detecting grid failed.");    
 
-            Eigen::Vector2f midpoint {0.0, 0.0};
-            rotate(M_PI / 2, driver);
+            float new_theta = theta_position + M_PI / 2;
+            new_theta = new_theta - floor(new_theta / M_PI * 2) *  M_PI * 2;
+
+            call_drive_to_service(x_position, y_position, new_theta, true);
+            ros::Duration(1).sleep();
 
             retry_count += 1;
             continue;
         }
 
         ROS_INFO("grid position: x_offset=%f, y_offset=%f, theta_offset=%f deg.", 
-            srv.response.x_offset, 
-            srv.response.y_offset, 
-            srv.response.theta_offset * 180 / M_PI);
+            detect_grid_srv.response.x_offset, 
+            detect_grid_srv.response.y_offset, 
+            detect_grid_srv.response.theta_offset * 180 / M_PI);   
 
-        Eigen::Vector2f midpoint {srv.response.x_offset, srv.response.y_offset};
-        align_at_position(midpoint, srv.response.theta_offset, driver);
+        float x_offset = detect_grid_srv.response.x_offset;
+        float y_offset = detect_grid_srv.response.y_offset;
+        float theta_offset = detect_grid_srv.response.theta_offset;
+
+        float x_global_offset = cos(theta_position) * x_offset - sin(theta_position) * y_offset;
+        float y_global_offset = sin(theta_position) * x_offset + cos(theta_position) * y_offset;
+        float theta_global_offset = theta_position + theta_offset;
         
+        call_drive_to_service(x_global_offset, y_global_offset, theta_global_offset, true);
         break;
     }
 
--- src/odometry/odometry.cpp
+++ src/odometry/odometry.cpp
@@ -66,7 +66,7 @@ void sensor_callback(const create_fundamentals::SensorPacket::ConstPtr& sensor_p
   theta = (theta + delta_theta);
   theta = theta - std::floor(theta / (2 * M_PI)) * (2 * M_PI);
 
-  ROS_INFO("\nx     = %f\ny     = %f\ntheta = %f", delta_x, delta_y, delta_theta);
+  ROS_INFO("\nx     = %f\ny     = %f\ntheta = %f", x, y, theta);
 
   last_left = new_left;
   last_right = new_right;
@@ -84,6 +84,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "odometry");
   ros::NodeHandle n;
 
+  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info)) {
   if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info)) {
     ros::console::notifyLoggerLevelsChanged();
   }
