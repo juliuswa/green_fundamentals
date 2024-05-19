018fce6ec82821e23f092e056267ecf8062572bb
Green Robot <juliuswa98@gmail.com>
2024-05-19 09:51:43 +0000

    recovery after broken repository

--- src/driving/driving_server.cpp
+++ src/driving/driving_server.cpp
@@ -2,14 +2,16 @@
 #include <boost/bind.hpp>
 #include <cmath>
 
+#include "../Eigen/Dense"
+
 #include "create_fundamentals/SensorPacket.h"
 #include "create_fundamentals/DiffDrive.h"
 #include "green_fundamentals/DriveTo.h"
 #include "green_fundamentals/Position.h"
 #include "../robot_constants.h"
 
-const float max_speed = 5.0;
-const float position_precision = 0.05;
+const float max_speed = 10.0;
+const float position_precision = 0.03;
 
 ros::ServiceClient diff_drive_service;
 
@@ -37,29 +39,40 @@ bool arrived(const green_fundamentals::Position::ConstPtr& position) {
 
 void drive_to_target(const green_fundamentals::Position::ConstPtr& position)
 { 
+    ROS_DEBUG("current position: x=%f, y=%f, theta=%f deg.", position->x, position->y, position->theta * 180 / M_PI);
+    ROS_DEBUG("current target: x=%f, y=%f, theta=%f", x_target, y_target, theta_target);
 
-    ROS_DEBUG("current target: x=%f, y=%f", x_target, y_target);
+    if (!arrived(position)) {
+        Eigen::Vector2f pos_delta {x_target - position->x, y_target - position->y};
+        ROS_INFO("not arrived. delta: (%f, %f)", pos_delta[0],  pos_delta[1]);
 
-    if (!arrived) {
-        ROS_INFO("not arrived. delta: x=%f, y=%f", x_target - position->x,  y_target - position->y);
+        Eigen::Vector2f robot_direction {cos(position->theta), sin(position->theta)};
+        ROS_INFO("robot_direction: (%f, %f)", robot_direction[0],  robot_direction[1]);       
 
-        float angle_to_dest = atan2(y_target - position->x, x_target - position->y); // in rad
-        float angle = angle_to_dest - position->theta;
-        angle = fmod(angle + M_PI, 2 * M_PI) - M_PI;  // norm it so it's between -180° and 180°
+        float angle = acos((pos_delta[0] * robot_direction[0] + pos_delta[1] * robot_direction[1]) / pos_delta.norm());
+        ROS_DEBUG("angle = %f deg.", angle * 180 / M_PI);
 
-        ROS_DEBUG("angle = %f", angle);
+        angle = std::min((float)(M_PI / 2.0), angle); // so it's between 0° and 90°
+        ROS_DEBUG("capped angle = %f deg.", angle * 180 / M_PI);  
 
-        float percent = angle / M_PI / 4;
-        float factor = -1 * (percent - 1);  // this will give 0 for 45°, -1 for 90° and 1 for 0°
+        float cross_product_z = pos_delta[0] * robot_direction[1] - pos_delta[1] * robot_direction[0];
+        ROS_DEBUG("cross_product_z = %f * %f - %f * %f = %f", 
+            pos_delta[0], robot_direction[1], pos_delta[1], robot_direction[0], cross_product_z);        
 
+        int direction = cross_product_z < 0 ? -1 : 1;
+        ROS_DEBUG("direction = %d", direction);        
 
-        WheelCommand command = {factor * max_speed, max_speed};
+        float factor = 1 - 2 * std::abs(angle) / (M_PI / 2);
+        ROS_DEBUG("factor = %f", factor);
 
-        if(angle < 0) {  // if right-turn
+        if (angle < 0) {
             WheelCommand command = {max_speed, factor * max_speed};
-        } 
-
-        drive(command);
+            drive(command);
+        }
+        else {
+            WheelCommand command = {factor * max_speed, max_speed};
+            drive(command);
+        }
     }
     else if (rotate) {
         ROS_INFO("rotating.");
@@ -90,7 +103,18 @@ bool set_target_position(green_fundamentals::DriveTo::Request  &req, green_funda
     x_target = req.x_target;
     y_target = req.y_target;
     rotate = req.rotate;
-    theta_target = req.theta_target;
+
+    float raw_theta = req.theta_target;
+
+    while (raw_theta < 0.0) { // Make positive
+        raw_theta = raw_theta + 2 * M_PI;
+    }
+
+    raw_theta = raw_theta - floor(raw_theta / (2.0 * M_PI)) * 2 * M_PI;  // modulo 2 pi
+    ROS_DEBUG("mod raw_theta=%f", raw_theta);
+
+    theta_target = raw_theta;
+    ROS_DEBUG("theta_target=%f", theta_target);
 
     return true;
 }
@@ -110,9 +134,7 @@ int main(int argc, char **argv)
 
     diff_drive_service = n.serviceClient<create_fundamentals::DiffDrive>("diff_drive");
 
-    while (ros::ok()) {
-        ros::spinOnce();
-    }
+    ros::spin();
 
     return 0;
 }
