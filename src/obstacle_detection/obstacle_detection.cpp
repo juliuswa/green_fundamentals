a9c06446d9a54b3a0e19b47ed6a8a667d0ec2301
Green Robot <juliuswa98@gmail.com>
2024-05-29 15:39:45 +0000

    obstacle_detection works

--- src/obstacle_detection/obstacle_detection.cpp
+++ src/obstacle_detection/obstacle_detection.cpp
@@ -2,37 +2,39 @@
 #include <list>
 
 #include "ros/ros.h"
+#include "../robot_constants.h"
 #include "green_fundamentals/Vector2f.h"
 #include "green_fundamentals/LaserCoordinates.h"
 #include "green_fundamentals/Obstacle.h"
 
 ros::Publisher obstacle_pub;
-float robot_radius = 0.335 / 2;
-float allowed_distance = 0.06;
+float robot_side = 0.08;  // determines at what y coordinate we are counting points to being on the side of the robot
+float allowed_distance = 0.1;  // how close to the wall we are allowed to go
 
 void detect_obstacle(const green_fundamentals::LaserCoordinates::ConstPtr& coordinates) { 
     green_fundamentals::Obstacle msg;
-    msg.left = false;
-    msg.front = false;
-    msg.right = false;
+    bool left = false;
+    bool front = false;
+    bool right = false;
 
-    for(green_fundamentals::Vector2f point : coordinates->coordinates) {
+    for(const green_fundamentals::Vector2f point : coordinates->coordinates) {
         float dist = std::sqrt(pow(point.x, 2) + pow(point.y, 2));
-
-        if(point.y > robot_radius) {
-            // left
-            msg.left = dist < (robot_radius + allowed_distance / 2) ? true : msg.left;
-        } else if(point.y < -robot_radius) {
-            // right
-            msg.right = dist < (robot_radius + allowed_distance / 2) ? true : msg.right;
-        } else {  
-            // front
-            msg.front = dist < (robot_radius + allowed_distance) ? true : msg.front;
-        }
-        ROS_DEBUG("dist: %f, x: %f, y: %f", dist, point.x, point.y);
-        ROS_DEBUG("left: %d, front: %d, rigt: %d", msg.left, msg.front, msg.right);
-    }
+        if(dist < CASING_RADIUS) continue;
     
+        if(point.y > robot_side) {
+            left = (dist < (CASING_RADIUS + allowed_distance / 2)) ? true : left;
+        } else if(point.y < -robot_side) {
+            right = (dist < (CASING_RADIUS + allowed_distance / 2)) ? true : right;
+        } else {
+            front = (dist < (CASING_RADIUS + allowed_distance)) ? true : front;
+        }
+    }
+
+    msg.left = left;
+    msg.front = front;
+    msg.right = right;
+    ROS_INFO("-------------------------------------");
+    ROS_INFO("left: %d, front: %d, rigt: %d", msg.left, msg.front, msg.right);
     obstacle_pub.publish(msg);
 }
 
@@ -42,7 +44,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     ros::Subscriber coordinates_sub = n.subscribe("laser_coordinates", 1, detect_obstacle);
-    obstacle_pub = n.advertise<green_fundamentals::LaserCoordinates>("obstacle", 1);
+    obstacle_pub = n.advertise<green_fundamentals::Obstacle>("obstacle", 1);
 
     ros::spin();
 
--- src/robot_constants.h
+++ src/robot_constants.h
@@ -6,4 +6,6 @@ const float WHEEL_BASE = 0.26; //m
 
 const float MAX_VELOCITY = 16; // rad/s?
 
+const float CASING_RADIUS = 0.1675;  // diameter is 33.5 cm
+
 #endif // CONSTANTS_H
